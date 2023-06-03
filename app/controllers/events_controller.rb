d846068a8149ffe7192a5c5440cf8a44f8e74580
marimo0283 <kugelschreiber1002@gmail.com>
2023-06-03 10:23:12 +0000

    [Add]イベント詳細、編集、削除機能

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,13 +8,36 @@ class EventsController < ApplicationController
   end
 
   def create
-    Event.create(event_parameter)
+    Event.create(event_params)
+    redirect_to root_path
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
     redirect_to root_path
   end
 
   private
 
-  def event_parameter
+  def event_params
     params.require(:event).permit(:title, :content, :start_time)
   end
 end
