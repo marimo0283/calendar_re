6c35a713a48bad84da2d858aba5970b38ae9dbb2
marimo0283 <kugelschreiber1002@gmail.com>
2023-06-09 00:35:49 +0000

    [Update]メモのバリデーション変更

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,6 @@ class Food < ApplicationRecord
   validates :morning, presence: true
   validates :noon, presence: true
   validates :night, presence: true
-  validates :memo, presence: true, length: { minimum: 1, maximum: 200 }
+  validates :memo, length: { minimum: 0, maximum: 200 }
   validates :start_time, presence: true
 end
